8e391a9a4d9ec7319e841d969ceca14f4b4e52a9
Kovas McCann <Kovasmccann@outlook.com>
2024-11-09 05:18:52 +0000

    Add debug function to Send.py for message logging

--- Code/Send.py
+++ Code/Send.py
@@ -5,6 +5,12 @@ parser.add_argument("file", help="The file to process")
 args = parser.parse_args()
 file = args.file
 
+def debug(message):
+    length = len(message).to_bytes(2, byteorder='big', signed=False)
+    type = '+'.encode('ascii')
+    message = message.encode('ascii')
+    print(f'{length} {type} {message}')
+
 def send(data):
     pass
 
@@ -15,7 +21,12 @@ def login_request(username, password, requested_session, requested_sequence_numb
     ## Will not be as secure as the real one
     pass
 
+debug("Fuck YOU bitch ass")
+
+exit()
+
 with open(file, 'rb') as f:
+    
     while True:
         # Read the message size (2 bytes)
         size_data = f.read(2)
